4bd22e41ae0bcd83c0e0b16190c392237f50f2b0
Benas Budrys <bud.benas@gmail.com>
2017-07-29 09:07:18 +0300

    Bound checking

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Map : MonoBehaviour {
 
     public static Map Instance;
 
     public enum Direction { Up, Down, Left, Right};
+    public Transform[] WalkableObjects;
+
+    private List<Vector2> territory = new List<Vector2>();
 
     private void Awake()
     {
@@ -14,22 +18,49 @@ public class Map : MonoBehaviour {
             Destroy(gameObject);
     }
 
+    private void Start()
+    {
+        for (int i = 0; i < WalkableObjects.Length; i++)
+        {
+            territory.Add(WalkableObjects[i].transform.position);
+        }
+    }
+
     public void Move(Transform target,Direction direction)
     {
+        Vector2 wantedPosition = target.position;
         switch (direction)
         {
             case Direction.Up:
-                target.position += Vector3.up;
+                wantedPosition += Vector2.up;
                 break;
             case Direction.Down:
-                target.position += Vector3.down;
+                wantedPosition += Vector2.down;
                 break;
             case Direction.Left:
-                target.position += Vector3.left;
+                wantedPosition += Vector2.left;
                 break;
             case Direction.Right:
-                target.position += Vector3.right;
+                wantedPosition += Vector2.right;
                 break;
         }
+        if (Walkable(wantedPosition))
+        {
+            target.position = wantedPosition;
+        }
+    }
+
+    private bool Walkable(Vector2 position)
+    {
+        bool canWalk = false;
+        for (int i = 0; i < territory.Count; i++)
+        {
+            if (territory[i] == position)
+            {
+                canWalk = true;
+                break;
+            }
+        }
+        return canWalk;
     }
 }
